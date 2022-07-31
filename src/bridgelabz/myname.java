597a7d16f9263578912a93098504dc0108a57884
javulm <javulmulla8@gmail.com>
2022-07-31 20:36:44 +0530

    [javul]program to show mya name

---
+++ src/bridgelabz/myname.java
@@ -0,0 +1,11 @@
+package bridgelabz;
+
+public class myname {
+public static void main(String[] args) {
+	System.out.println(" ********    ***   **       ** **     **  **");
+	System.out.println("    **      ** **   **     **  **     **  **");
+	System.out.println("    **     *******   **   **   **     **  **");
+	System.out.println(" *  **    **     **   ** **    **     **  **");
+	System.out.println("  ***    **       **   ***      *******   *******");
+}
+}
